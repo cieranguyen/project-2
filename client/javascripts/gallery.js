e6ae0e97f31f88fe9221f30b1157458bd59791f9
Ciera Nguyen <cieran@uoregon.edu>
2020-04-22 15:27:24 -0700

    Lab Week 3: The Articles API

--- client/javascripts/gallery.js
+++ client/javascripts/gallery.js
@@ -1,15 +1,17 @@
 // jshint esversion: 6
 
-let main = function(){
+let controller = function(){
 
-    let prefixURL = "http://api.flickr.com/services/feeds/photos_public.gne?tags=";
+    let prefixURL = "https://api.flickr.com/services/feeds/photos_public.gne?tags=";
     let suffixURL = "&format=json&jsoncallback=?";
     //get value entered by user from textbox
-    let flickrTag = $("input").???();
+     let flickrTag = document.querySelector("input[type=text]").value;
+  //  let flickerTag = $("input").img();
     let requestURL = prefixURL + flickrTag + suffixURL;
 
     //clear old photos
-    $(".photos").???("");
+    document.querySelector(".photos").innerHTML = "";
+//    $(".photos").hide("");
 
   $.getJSON(requestURL, function(flickrResponse) {
     flickrResponse.items.forEach(function(item, index) {
@@ -37,4 +39,11 @@ let main = function(){
 };
 
 
-$(document).ready(main);
+// $(document).ready(controller);
+//Register the controller after the DOM is complete
+window.addEventListener("load", function() {
+  //select the button
+  let button = document.querySelector("button");
+  //register the click handler for the button
+  button.addEventListener("click", controller);
+});
