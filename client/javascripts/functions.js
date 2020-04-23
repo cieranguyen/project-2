a53df4f85a5066cd47fbd03bb4170f5fc015b919
Ciera Nguyen <cieran@uoregon.edu>
2020-04-23 15:40:54 -0700

    Function Practice

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -14,5 +14,32 @@ let containsDuplicates = s => {
   }
   return false;
 };
-
-let revWords1 = (word) => {
+//accepts a sentence or phrase (a string) and returns a string with the words in s reversed. Words are delimited by a single space. Use a for
+ // loop, and arrow syntax. Hint: String.split method.
+let revWords1 = (str) => {
+let arr = sent.split(" ");
+let result = "";
+for (var i = 0; i < arr.length; i++) {
+  revWord(arr[i])
+} 
+return result
+}
+//for.Each
+let revWords2 = (str) => {
+  //turn string into array
+  let arr = str.split("");
+  let result = "";
+  str.forEach((char)=>{
+    result = char + result;
+  })
+  return result;
+}
+//for.. of loop
+let revWord3 = (str) => {
+  let arr = str.split("");
+  let result = "";
+  for (char of arr){
+    result = char + result;
+  }
+  return result;
+}
